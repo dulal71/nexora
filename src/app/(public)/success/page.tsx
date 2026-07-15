8ef32e30e8e0779b8f840e7d7396a711ae66ee82
dulal71 <ahmeddulal4211@gmail.com>
2026-07-16 00:17:18 +0600

    fixed email issue

--- src/app/(public)/success/page.tsx
+++ src/app/(public)/success/page.tsx
@@ -1,43 +1,49 @@
-import { stripe } from '@/lib/stripe'
-import { redirect } from 'next/navigation'
+import { stripe } from "@/lib/stripe";
+import { redirect } from "next/navigation";
 
 interface SuccessPageProps {
   searchParams: Promise<{
     session_id?: string;
-    
   }>;
 }
 
-export default async function Success({ searchParams }:SuccessPageProps) {
-  const { session_id } = await searchParams
+export default async function Success({
+  searchParams,
+}: SuccessPageProps) {
+  const { session_id } = await searchParams;
 
-  if (!session_id)
-    throw new Error('Please provide a valid session_id (`cs_test_...`)')
-
-  const {
-    status,
-    customer_details: { email: customerEmail }
-  } = await stripe.checkout.sessions.retrieve(session_id, {
-    expand: ['line_items', 'payment_intent']
-  })
-
-  if (status === 'open') {
-    return redirect('/')
+  if (!session_id) {
+    throw new Error("Please provide a valid session_id (`cs_test_...`)");
   }
 
-  if (status === 'complete') {
+  const session = await stripe.checkout.sessions.retrieve(session_id, {
+    expand: ["line_items", "payment_intent"],
+  });
+
+  const status = session.status;
+  const customerEmail = session.customer_details?.email ?? "";
+
+  if (status === "open") {
+    redirect("/");
+  }
+
+  if (status === "complete") {
     return (
-      <section id="success" className="p-8 border border-gray-200 rounded-lg max-w-lg mx-auto mt-10">
-      <h2 className="text-2xl font-bold mb-4">Thank You!</h2>
-      <p className="text-gray-700">
-        We appreciate your business! A confirmation email will be sent to{' '}
-        <span className="font-semibold">{customerEmail}</span>. 
-        If you have any questions, please email{' '}
-        <a href="mailto:orders@example.com" className="text-blue-600 underline">
-          orders@example.com
-        </a>.
-      </p>
-    </section>
-    )
+      <section>
+        <h1>Thank You!</h1>
+
+        <p>
+          We appreciate your business! A confirmation email will be sent to{" "}
+          <strong>{customerEmail}</strong>.
+        </p>
+
+        <p>
+          If you have any questions, please email{" "}
+          <a href="mailto:orders@example.com">orders@example.com</a>.
+        </p>
+      </section>
+    );
   }
+
+  return null;
 }
