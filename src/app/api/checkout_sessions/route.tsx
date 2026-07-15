5ebe6b0ebe8df4a08738cff25370ab81caa4e6b0
dulal71 <ahmeddulal4211@gmail.com>
2026-07-16 00:35:23 +0600

    fixed checkout page issue

--- src/app/api/checkout_sessions/route.tsx
+++ src/app/api/checkout_sessions/route.tsx
@@ -9,8 +9,14 @@ export async function POST(request: NextRequest) {
     const headersList = await headers()
     const origin = headersList.get('origin')
      const formData = await request.formData()
-     const price = formData.get('price')
+     const price = Number(formData.get("price"));
      const user = await getSession()
+     if (!user) {
+  return NextResponse.json(
+    { error: "Unauthorized" },
+    { status: 401 }
+  );
+}
    
     const session = await stripe.checkout.sessions.create({
        customer_email:user?.email,
@@ -26,21 +32,33 @@ export async function POST(request: NextRequest) {
     quantity: 1,
   },
 ],
-metadata:{
-  price:price,
-  name:user.name,
-  userId:user.id,
-  status:'Completed',
-  userEmail:user.email
+metadata: {
+  price: String(price),
+  name: user.name ?? "",
+  userId: user.id,
+  status: "Completed",
+  userEmail: user.email ?? "",
 },
       mode: 'payment',
       success_url: `${origin}/success?session_id={CHECKOUT_SESSION_ID}`,
     });
-    return NextResponse.redirect(session.url, 303)
-  } catch (err) {
+   if (!session.url) {
+  throw new Error("Stripe session URL not found");
+}
+
+return NextResponse.redirect(session.url, 303);
+
+} catch (err: unknown) {
+  if (err instanceof Error) {
     return NextResponse.json(
       { error: err.message },
-      { status: err.statusCode || 500 }
-    )
+      { status: 500 }
+    );
   }
+
+  return NextResponse.json(
+    { error: "Something went wrong" },
+    { status: 500 }
+  );
+}
 }
