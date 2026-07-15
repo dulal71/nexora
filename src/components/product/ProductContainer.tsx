49e87268022b960ce23d83f7a311cbda48c1d0fd
dulal71 <ahmeddulal4211@gmail.com>
2026-07-16 00:46:05 +0600

    fixem price type issue

--- src/components/product/ProductContainer.tsx
+++ src/components/product/ProductContainer.tsx
@@ -11,7 +11,7 @@ export interface Product {
   name: string;
  brand: string;
   price: number;          
-  discountPrice?: string; 
+  discountPrice?: number; 
  images: string[];        
 }
 interface ProductContainerProps {
