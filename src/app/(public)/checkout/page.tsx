f53c033b020db0b88d1ecc74f9e3cbe687b75880
dulal71 <ahmeddulal4211@gmail.com>
2026-07-23 19:07:57 +0600

    fixed taxtarea issue

--- src/app/(public)/checkout/page.tsx
+++ src/app/(public)/checkout/page.tsx
@@ -84,7 +84,7 @@ const CheckoutPage = () => {
             <textarea
               name="address"
               value={formData.address}
-              onChange={handleChange}
+             
               rows={3}
               className="w-full border rounded-md px-3 py-2 text-sm dark:bg-black dark:text-white"
               placeholder="House, Road, Area"
